7b6d1e170f78d05a589b04377e1be23ed6d5ab7e
Meghthedev <meghthedev@gmail.com>
2023-12-29 07:03:03 +0530

    ipc: Import OEM Changes
    Signed-off-by: Meghthedev <meghthedev@gmail.com>

--- ipc/util.c
+++ ipc/util.c
@@ -86,6 +86,7 @@ static int __init ipc_init(void)
 {
 	int err_sem, err_msg;
 
+	proc_mkdir("sysvipc", NULL);
 	err_sem = sem_init();
 	WARN(err_sem, "ipc: sysv sem_init failed: %d\n", err_sem);
 	err_msg = msg_init();
@@ -409,8 +410,8 @@ static int ipcget_public(struct ipc_namespace *ns, struct ipc_ids *ids,
 static void ipc_kht_remove(struct ipc_ids *ids, struct kern_ipc_perm *ipcp)
 {
 	if (ipcp->key != IPC_PRIVATE)
-		WARN_ON_ONCE(rhashtable_remove_fast(&ids->key_ht, &ipcp->khtnode,
-				       ipc_kht_params));
+		rhashtable_remove_fast(&ids->key_ht, &ipcp->khtnode,
+				       ipc_kht_params);
 }
 
 /**
@@ -425,7 +426,7 @@ void ipc_rmid(struct ipc_ids *ids, struct kern_ipc_perm *ipcp)
 {
 	int lid = ipcid_to_idx(ipcp->id);
 
-	WARN_ON_ONCE(idr_remove(&ids->ipcs_idr, lid) != ipcp);
+	idr_remove(&ids->ipcs_idr, lid);
 	ipc_kht_remove(ids, ipcp);
 	ids->in_use--;
 	ipcp->deleted = true;
